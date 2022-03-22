f6a421e410eeb41e241514ed7a91e5e831b0fdf3
hoagnhat <hahng.nhat@gmail.com>
2022-03-22 22:28:49 +0700

    Fix cors

--- src/main/java/com/edu/capstone/common/config/AppConfig.java
+++ src/main/java/com/edu/capstone/common/config/AppConfig.java
@@ -63,7 +63,7 @@ public class AppConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
 			}
 		};
 	}
