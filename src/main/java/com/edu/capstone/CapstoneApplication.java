d76a54e7adf8532273151e7dcde72070731460bc
hoagnhat <hahng.nhat@gmail.com>
2022-03-28 01:49:13 +0700

    fix

--- src/main/java/com/edu/capstone/CapstoneApplication.java
+++ src/main/java/com/edu/capstone/CapstoneApplication.java
@@ -4,6 +4,7 @@ import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -13,14 +14,20 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 @SpringBootApplication
 @OpenAPIDefinition
 public class CapstoneApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneApplication.class, args);
+	}
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(CapstoneApplication.class);
 	}
-
+	
 	@PostConstruct
 	public void init() {
 		// Setting Spring Boot SetTimeZone
 		TimeZone.setDefault(TimeZone.getTimeZone("GMT+7:00"));
 	}
+
 }
