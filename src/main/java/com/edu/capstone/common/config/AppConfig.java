ec5853fe3351c8836541346218e091097faf1e79
Ng-uyen Nguyen <nguyenndb511@gmail.com>
2022-03-31 22:04:24 +0700

    Fix bug

--- src/main/java/com/edu/capstone/common/config/AppConfig.java
+++ src/main/java/com/edu/capstone/common/config/AppConfig.java
@@ -15,11 +15,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * @author NhatHH
- * Date: Jan 30, 2022
+ * @author NhatHH Date: Jan 30, 2022
  */
 @Configuration
 @EnableWebMvc
@@ -29,37 +30,47 @@ public class AppConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	public JavaMailSender javaMailSender() {
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 		mailSender.setHost("smtp.gmail.com");
 		mailSender.setPort(587);
-		
+
 		mailSender.setUsername("capstone.help.center@gmail.com");
 		mailSender.setPassword("kggihtkxmrldixws");
-		
+
 		Properties props = mailSender.getJavaMailProperties();
 		props.put("mail.transport.protocol", "smtp");
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.debug", "true");
-		
+
 		return mailSender;
 	}
-	
+
 	@Bean
-	  public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
-	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowCredentials(true);
-	    config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-	    config.setAllowedMethods(Collections.singletonList("*"));
-	    config.setAllowedHeaders(Collections.singletonList("*"));
-	    source.registerCorsConfiguration("/**", config);
-	    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-	    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-	    return bean;
-	  }
-	
+	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// TODO Auto-generated method stub
+				registry.addMapping("/**").allowedMethods("*").allowedHeaders("*").allowedOrigins("*");
+			}
+		};
+	}
 }
